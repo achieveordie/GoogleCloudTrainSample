34019cd23ef5911e1cf4601697f02da124373f3a
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2022-10-20 16:46:52 +0530

    add setup and requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages
+from setuptools import setup
+
+DEPENDENCIES = [
+    "google-api-python-client",
+    "numpy==1.23.4",
+    "scikit-learn==1.0.2",
+    "tensorflow==2.8.0",
+]
+EXTRA_DEPENDENCIES = []
+
+setup(
+    name="SampleTrainer",
+    version="0.1",
+    description="A small example of how to train on gcp",
+    author="Sagar Mishra",
+    install_requires=DEPENDENCIES,
+    extra_require=EXTRA_DEPENDENCIES,
+    python_reqiures=">3.6",
+    packages=find_packages(),
+    include_package_data=True,
+)
