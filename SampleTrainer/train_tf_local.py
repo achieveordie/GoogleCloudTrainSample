d348a94124bc04cd7f3e6b39fb9c34c4c0524d91
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2022-11-21 11:19:44 +0530

    add local tensorflow training

--- SampleTrainer/train_tf_local.py
+++ SampleTrainer/train_tf_local.py
@@ -1,28 +1,31 @@
 import argparse
+import logging
 
 import tensorflow as tf
 
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from utils import get_data, save_file
+from SampleTrainer import get_data
 
-RANDOM_STATE=10
-SAVE_LOCATION = Path().cwd().parents[0] / "models/tf/"
+RANDOM_STATE = 10
+SAVE_LOCATION = Path.cwd().parents[0] / "models/tf/"
 SAVE_LOCATION.mkdir(exist_ok=False, parents=True)
 
 
 def train(save_location, verbose):
+    logging.info("[Logged]Starting training, given save loc: ", save_location)
     if verbose:
         print("Given location is: ", save_location)
 
     X, y = get_data(train=True)
     keras_model = tf.keras.models.Sequential([
-        tf.keras.layers.Input(shape=(X.shape[1], )),
+        tf.keras.layers.InputLayer(input_shape=(X.shape[1], )),
         tf.keras.layers.Dense(32, activation="relu"),
         tf.keras.layers.Dense(4, activation="relu"),
         tf.keras.layers.Dense(1, activation="sigmoid"),
     ])
 
+    logging.info("[Logged]Model has been built.")
     if verbose:
         print(keras_model.summary())
 
@@ -32,16 +35,19 @@ def train(save_location, verbose):
         metrics="accuracy",
     )
 
+    logging.info("[Logged]Model has been compiled.")
+
     keras_model.fit(X, y, epochs=3, verbose=verbose)
-    # keras_model.save(save_location / "keras_model/")    # !Temporary!
+    logging.info("Model has been fitted.")
+
     with TemporaryDirectory() as tmp_dir:
         temp_save_loc = Path(tmp_dir) / "tf/"
-        keras_model.save(temp_save_loc)
+        keras_model.save(str(temp_save_loc))
 
         for element in temp_save_loc.iterdir():
             if element.is_dir():
                 file = save_location / element.name
-                file.mkdir()
+                tf.io.gfile.makedirs(str(file))
 
         for dir, sub_dir, files in tf.io.gfile.walk(temp_save_loc):
             for file in files:
@@ -49,13 +55,18 @@ def train(save_location, verbose):
                 parent_dir = file_loc.parents[0]
                 if parent_dir.name != "tf":
                     # change storage location if not the root directory
-                    file = parent_dir.relative_to(file_loc.parents[1]) / file
-                tf.io.gfile.copy(file_loc, save_location / file)
+                    file = parent_dir.relative_to(file_loc.parents[1]) / str(file)
+                tf.io.gfile.copy(str(file_loc), str(save_location / str(file)))
+
+    logging.info("[Logged]Model has been saved to the given location.")
 
 
 if __name__ == "__main__":
+    logging.basicConfig()
+    logging.info("[Logged]Starting parsing arguments")
+
     parser = argparse.ArgumentParser(
-        description="Train Simple NN on dummy data"
+        description="Locally train Simple NN on dummy data"
     )
 
     parser.add_argument(
@@ -74,4 +85,6 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    logging.info(f"[Logged]Parsed arguments: {args}")
+
     train(args.location, int(args.verbose))
