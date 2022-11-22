2521bcf27ff4092cb389184201a6ca77c6e301b7
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2022-11-22 11:16:28 +0530

    update and link exact package versions

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import find_packages
 from setuptools import setup
 
 DEPENDENCIES = [
-    "google-api-python-client",
+    "gcsfs==2022.2.0",
+    "google-api-python-client==2.65.0",
     "scikit-learn==0.24.2",
     "tensorflow==2.6",
     "keras==2.6.0",
-    "transparentpath==1.1.32",
 ]
 EXTRA_DEPENDENCIES = []
 
+description = "An end-to-end example on how to train on GCP's AI-Platform"
 setup(
     name="SampleTrainer",
     version="0.1",
-    description="A small example of how to train on gcp",
+    description=description,
     author="Sagar Mishra",
     install_requires=DEPENDENCIES,
     extra_require=EXTRA_DEPENDENCIES,
